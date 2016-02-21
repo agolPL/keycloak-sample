e1ec911c66cf8d0840f156b417bac278ece4e64a
agolPl <andipansa@gmail.com>
2016-02-21 12:38:49 +0100

    view for briefing

--- briefing/src/main/java/pl/agol/briefing/flightplan/FlightPlanController.java
+++ briefing/src/main/java/pl/agol/briefing/flightplan/FlightPlanController.java
@@ -2,14 +2,18 @@ package pl.agol.briefing.flightplan;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.validation.Valid;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 @RequestMapping(value = "/flightplan")
-public class FlightPlanController {
+public class FlightPlanController extends WebMvcConfigurerAdapter {
 
     private final FlightPlanService flightPlanService;
 
@@ -19,13 +23,18 @@ public class FlightPlanController {
     }
 
     @RequestMapping(method = GET)
-    public String flightPlanForm() {
+    public String flightPlanForm(FlightPlanRequest flightPlanRequest) {
         return "flight-plan";
     }
 
 
     @RequestMapping(method = POST)
-    public String submitFlightPlanForm(FlightPlanSubmitRequest flightPlanSubmitRequest) {
+    public String submitFlightPlanForm(@Valid FlightPlanRequest flightPlanRequest,
+                                       BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "flight-plan";
+        }
         return "success";
     }
 }
--- briefing/src/main/java/pl/agol/briefing/flightplan/FlightPlanService.java
+++ briefing/src/main/java/pl/agol/briefing/flightplan/FlightPlanService.java
@@ -4,4 +4,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FlightPlanService {
+
+    public FlightPlanValidationResult validate(String flightPlan) {
+
+        //TODO: use validate flight-plan service
+        return new FlightPlanValidationResult();
+    }
+
+    public void submit(FlightPlanRequest flightPlanRequest) {
+
+    }
 }
