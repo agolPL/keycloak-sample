38fd44a1dd95a96dace8ac70ba985a273c69be79
agolPl <andipansa@gmail.com>
2016-02-22 23:41:56 +0100

    add logging

--- flightplan-parser/src/main/java/pl/agol/fplparser/FlightPlanParserController.java
+++ flightplan-parser/src/main/java/pl/agol/fplparser/FlightPlanParserController.java
@@ -1,5 +1,6 @@
 package pl.agol.fplparser;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@Slf4j
 @RestController
 public class FlightPlanParserController {
 
@@ -19,6 +21,7 @@ public class FlightPlanParserController {
 
     @RequestMapping(value = "/api/flightplan/parse", method = POST)
     public FlightPlan parse(@RequestBody String flightPlanDocument) {
+        log.info("Received flight plan to parse: " + flightPlanDocument);
         return flightPlanParser.parse(flightPlanDocument);
     }
 }
