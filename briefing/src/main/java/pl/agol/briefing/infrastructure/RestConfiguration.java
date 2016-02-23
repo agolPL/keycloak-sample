f2230efe76c53ff967fe3297a8ba0513badfc3c8
agolPl <andipansa@gmail.com>
2016-02-23 23:18:24 +0100

    secure ommunication between briefing and receiver

--- briefing/src/main/java/pl/agol/briefing/infrastructure/RestConfiguration.java
+++ briefing/src/main/java/pl/agol/briefing/infrastructure/RestConfiguration.java
@@ -1,14 +1,22 @@
 package pl.agol.briefing.infrastructure;
 
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class RestConfiguration {
 
+    @Autowired
+    KeycloakClientRequestFactory keycloakClientRequestFactory;
+
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public KeycloakRestTemplate keycloakRestTemplate() {
+        return new KeycloakRestTemplate(keycloakClientRequestFactory);
     }
 }
