9f9043f97b87eb344930b936d4c5914759282cc2
agolPl <andipansa@gmail.com>
2016-02-23 18:03:22 +0100

    unsecure briefing

--- briefing/src/main/java/pl/agol/briefing/infrastructure/RestConfiguration.java
+++ briefing/src/main/java/pl/agol/briefing/infrastructure/RestConfiguration.java
@@ -1,22 +1,14 @@
 package pl.agol.briefing.infrastructure;
 
-import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
-import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestConfiguration {
 
-    @Autowired
-    public KeycloakClientRequestFactory keycloakClientRequestFactory;
-
     @Bean
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public KeycloakRestTemplate keycloakRestTemplate() {
-        return new KeycloakRestTemplate(keycloakClientRequestFactory);
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
